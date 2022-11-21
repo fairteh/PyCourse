03abf34bef4137e22a4ce863e83a98750e8f74cb
fairteh <glebshemchuk@gmail.com>
2022-11-21 21:30:55 +0300

    PersonalTevirp was added. V.1.0.

---
+++ PersonalTevirp.py
@@ -0,0 +1,7 @@
+#персональный привет от петра (кто такой Петр?)
+#работаем на пользовательский ввод, проще говоря инпут от пользоваетля
+
+name = input("Hello! What's your name? (say him that your name is Tony)")
+print("Fuck you,", name + "!")
+print("\n\nhttps://www.youtube.com/watch?v=tSrH-6YUf1g)))")
+input("\n\nНажмите Enter, чтобы закрыть свой рот и выйти!")
